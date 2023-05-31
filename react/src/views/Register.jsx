f1921bca289a2f79481ae429d8226c6442e552eb
Imad Ahmed <imadahmed101@gmail.com>
2023-05-31 06:05:58 -0400

    first commit

---
+++ react/src/views/Register.jsx
@@ -0,0 +1,64 @@
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axiosClient from '../axios-client'
+import { useStateContext } from '../contexts/ContextProvider'
+
+function Register() {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+    const [errors, setErrors] = useState(null)
+
+    const {setUser, setToken} = useStateContext()
+
+    const onSubmit = (ev) => {
+        ev.preventDefault()
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        }
+        console.log(payload)
+        axiosClient.post('/register', payload)
+        .then(({data}) => {
+            setUser(data.user)
+            setToken(data.token)
+        })
+        .catch(err => {
+            const response = err.response;
+            if (response && response.status === 422) {
+                setErrors(response.data.errors)
+            }
+        })
+    }
+    return (
+        <div className='login-signup-form animated fadeInDown'>
+            <div className='form'>
+                <form onSubmit={onSubmit}>
+                    <h1 className='title'>Register for an account</h1>
+                    {
+                        errors && <div className='alert'>
+                            {Object.keys(errors).map(key => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    }
+                    <input ref={nameRef} placeholder='Full Name' />
+                    <input ref={emailRef} type='email' placeholder='Email Address' />
+                    <input ref={passwordRef} type='password' placeholder='Password' />
+                    <input ref={passwordConfirmationRef} type='password' placeholder='Password Confirmation' />
+                    <button className='btn btn-block'>Register</button>
+                    <p className='message'>
+                        Already Registered? <Link to='/login'>Login to your account</Link>
+                    </p>
+                </form>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Register
